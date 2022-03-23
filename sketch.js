0feb06f8254606bc59bf1cbf89d90ecc79c1758b
BelowAvgStudent <98503149+BelowAvgStudent@users.noreply.github.com>
2022-03-22 18:53:39 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var box
+
+function setup() {
+  createCanvas(400,400);
+box= createSprite(200,200,10,10)
+}
+
+function draw() 
+{
+  background("pink");
+  if(keyIsDown(RIGHT_ARROW)){
+    box.x=box.x+5
+  }
+  if(keyIsDown(LEFT_ARROW)){
+    box.x-=5
+  }
+  if(keyIsDown(UP_ARROW)){
+    box.y-=5
+  }
+  if(keyIsDown(DOWN_ARROW)){
+    box.y+=5
+  }
+  box.shapeColor="black"
+drawSprites()}
+
+
+
+
+
